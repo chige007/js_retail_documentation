688d301eb2862d145dc6c171e3abdb18c0f56fcc
chige007 <461841500@qq.com>
2018-10-25 17:45:22 +0800

    init

---
+++ src/index.js
@@ -0,0 +1,29 @@
+import Vue from 'vue';
+
+import 'vx-easyui/dist/themes/default/easyui.css';
+import 'vx-easyui/dist/themes/icon.css';
+import 'vx-easyui/dist/themes/vue.css';
+import EasyUI from 'vx-easyui';
+import locale from 'vx-easyui/dist/locale/easyui-lang-zh_CN'
+
+import app from './app.vue';
+import './styles/common.less';
+
+Vue.use(EasyUI, {
+    locale: locale
+});
+
+new Vue({
+    el : "#MYAPP",
+	data : () => {
+        return {
+        }
+	},
+	render: (h) => h(app)
+});
+
+if(process.env.NODE_ENV == "development"){
+    console.log("当前是开发模式！");
+}else if(process.env.NODE_ENV == "production"){
+    console.log("当前是生产模式！");
+}
